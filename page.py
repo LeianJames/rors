5f250a9ee7123822a78617fba726059ee6fd7c00
Lei <leijames28@gmail.com>
2025-08-06 07:52:24 +0800

    LSM

---
+++ page.py
@@ -0,0 +1,18 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("This is the title of the game")
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+#draw all other elements
+    
+    pygame.display.update()
+# This is a comment
+# This is another comment
+# This is a third comment
